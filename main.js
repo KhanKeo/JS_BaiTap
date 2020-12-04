add39ef3045adaf33598acc9bf369693bc666ade
KHanKeo <Khankeo9992016@gmail.com>
2020-12-04 14:54:43 +0700

    jj

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+function onClick(){
+    console.log('clicked');
+    let el = document.querySelector("button");
+    el.classList.remove("li");
+    let h2 = document.querySelector("h2");
+    h2.classList.add("hidden");
+    
+}
+let el = document.querySelector("button");
+el.addEventListener('click', onClick);
 
