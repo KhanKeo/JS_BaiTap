9825412d33884dab830df806d4af49b84d5450df
KHanKeo <Khankeo9992016@gmail.com>
2020-12-08 19:24:55 +0700

    BT5

--- main.js
+++ main.js
@@ -21,15 +21,15 @@ function newElement() {
    // tạo node li
    let li = document.createElement("li");
    // lấy giá trị từ input
-   let inputValue = document.getElementById("myInput").value;
+   let inputValue = document.getElementById("idInput").value;
    // tạo đối tượng text node
    let t = document.createTextNode(inputValue);
    // gán text node cho thẻ li vừa tạo
    li.appendChild(t);
    //thêm vào trong thẻ li
-   document.getElementById("myUL").appendChild(li);
+   document.getElementById("idUL").appendChild(li);
    // xóa giá trị thẻ input
-   document.getElementById("myInput").value = "";
+   document.getElementById("idInput").value = "";
    // thêm button xóa vào thẻ li mới tạo
    let span = document.createElement("span");
    let txt = document.createTextNode("\u00D7");
@@ -40,14 +40,14 @@ function newElement() {
    for (let i = 0; i < close.length; i++) {
       close[i].onclick = function() {
          let div = this.parentElement;
-         div.style.display = "none";
+         div.style.display = "none"; 
       }
    }
 }
 
 //xóa toàn bộ
-let clear = document.getElementById("clear");
+let clear = document.getElementById("idClear");
 clear.addEventListener("click", function () {
-    let ulList = document.getElementById("myUL");
+    let ulList = document.getElementById("idUL");
     ulList.innerHTML = "";
  })
