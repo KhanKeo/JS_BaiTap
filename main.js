53adb069c838c60890b243b8ea70b3cb216e2dc5
KHanKeo <Khankeo9992016@gmail.com>
2020-12-08 18:54:47 +0700

    BT_JS

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
-function onClick(){
-    console.log('clicked');
-    let el = document.querySelector("button");
-    el.classList.remove("li");
-    let h2 = document.querySelector("h2");
-    h2.classList.add("hidden");
-    
+let myNodelist = document.getElementsByTagName("li");
+for ( let i = 0; i < myNodelist.length; i++) {
+   let span = document.createElement("span");
+   let txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   myNodelist[i].appendChild(span);
 }
-let el = document.querySelector("button");
-el.addEventListener('click', onClick);
+// Khi button xóa được click thì ẩn phần tử li chứa nó
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+   close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+   }
+}
 
+
+// Thêm công việc mới khi click vào button "thêm"
+function newElement() {
+   // tạo node li
+   let li = document.createElement("li");
+   // lấy giá trị từ input
+   let inputValue = document.getElementById("myInput").value;
+   // tạo đối tượng text node
+   let t = document.createTextNode(inputValue);
+   // gán text node cho thẻ li vừa tạo
+   li.appendChild(t);
+   //thêm vào trong thẻ li
+   document.getElementById("myUL").appendChild(li);
+   // xóa giá trị thẻ input
+   document.getElementById("myInput").value = "";
+   // thêm button xóa vào thẻ li mới tạo
+   let span = document.createElement("span");
+   let txt = document.createTextNode("\u00D7");
+   span.className = "close";
+   span.appendChild(txt);
+   li.appendChild(span);
+   // gán lại sự kiện cho các button xóa
+   for (let i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+         let div = this.parentElement;
+         div.style.display = "none";
+      }
+   }
+}
+
+//xóa toàn bộ
+let clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+    let ulList = document.getElementById("myUL");
+    ulList.innerHTML = "";
+ })
